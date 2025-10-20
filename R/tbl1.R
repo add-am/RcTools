40e4c255218a1cd43c801dc1f9cd93ee9e5c93b3
Adam Shand <adamshand22@gmail.com>
2025-10-20 16:03:44 +1000

    add documentation for datasets, and add datasets

---
+++ R/tbl1.R
@@ -0,0 +1,25 @@
+#' Hypothetical Technical Report Data 1
+#'
+#' A completely made up dataset containing all possible scores 
+#' (and some impossible scores) that might occur in one of the Northern Three
+#' Technical Reports.
+#'
+#' @format ## `tbl1`
+#' A dataframe with 9 rows and 12 columns:
+#' \describe{
+#'   \item{V1}{Variable 1, a numeric score}
+#'   \item{V2}{Variable 2, a numeric score}
+#'   \item{V3}{Variable 3, a numeric score}
+#'   \item{V4}{Variable 4, a numeric score}
+#'   \item{V5}{Variable 5, a numeric score}
+#'   \item{V6}{Variable 6, a numeric score}
+#'   \item{V7}{Variable 7, a numeric score}
+#'   \item{V8}{Variable 8, a numeric score}
+#'   \item{V9}{Variable 9, a numeric score}
+#'   \item{V10}{Variable 10, a numeric score}
+#'   \item{V11}{Variable 11, a numeric score}
+#'   \item{V12}{Variable 12, a numeric score}
+#'   ...
+#' }
+#' @source NA - this is a completely made up dataset.
+"tbl1"
