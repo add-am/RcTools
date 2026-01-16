a45bc991fb5da5d70505ba270f1c789e5ccbdcab
Adam Shand <adamshand22@gmail.com>
2026-01-16 12:28:59 +1000

    add qld dataset

---
+++ R/qld.R
@@ -0,0 +1,11 @@
+#' Queensland State Border
+#'
+#' An outline of the Queensland State
+#'
+#' @format ## `qld`
+#' An sf object with 883 rows and 21 columns:
+#' \describe{
+#'   ...
+#' }
+#' @source extracted out of the GISAIMSR Package
+"qld"
