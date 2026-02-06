fc6f94f329b6fa3fd40bdb8151ba794f2653bae9
Adam Shand <adams@townsvilleport.com.au>
2026-02-06 14:14:13 +1000

    make quarto fetch func

---
+++ R/get_quarto_params.R
@@ -0,0 +1,19 @@
+#' Return the YAML params during an interative session
+#'
+#' @returns a list of all YAML parameters
+#'
+#' @export
+#' @examples
+#' 
+#' params <- get_quarto_params()
+get_quarto_params <- function(){
+
+  #if the session calling this function is interactive
+  if (interactive()) {
+    params <- rmarkdown::yaml_front_matter(rstudioapi::getSourceEditorContext()$path)$params
+  }
+
+  #return the params object
+  return(params)
+
+}
